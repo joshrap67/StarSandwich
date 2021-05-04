40642a32c83b6fd4ff20f1889c938ef9c90a209f
joshrap67 <ghosthippo67@gmail.com>
2021-05-04 08:32:19 -0400

    More cleaning

--- tools/sky_checker.py
+++ tools/sky_checker.py
@@ -4,13 +4,32 @@ import math
 import os
 
 RA_OFFSET = (0.25) * 2  # in hours
-DEC_OFFSET = (2.5) * 2  # in degrees
+DEC_OFFSET = (2.5124) * 2  # in degrees
 DEC_ORIGIN = -90
 MAX_RA_BIN = math.ceil(24.0 / RA_OFFSET)  # starting from bin = 0 of course
 MAX_DEC_BIN = math.ceil(180.0 / DEC_OFFSET)  # starting from bin = 0 of course
 
 
 def determine_precision():
+    histogram = get_histogram()
+
+    bad_points = get_number_of_bad_points(histogram)
+    total_points = MAX_DEC_BIN * MAX_RA_BIN
+    percentage = (bad_points / total_points) * 100
+    RA_to_degrees = (RA_OFFSET / 2) * 15
+
+    os.system('cls')
+    print("----------------Finished computing----------------")
+    print("Right ascension range: {} hrs ({}°)".format(
+        RA_OFFSET / 2, RA_to_degrees))
+    print("Declination range: {}°".format(DEC_OFFSET / 2))
+    print("Total squares that do not have any stars from the database: {}".format(
+        bad_points))
+    print("Total squares the sky was split into: {}".format(total_points))
+    print("Percentage of squares without stars: {}%".format(percentage))
+
+
+def get_histogram():
     histogram_3d = list()
     histogram_3d = [[[] for x in range(MAX_DEC_BIN)]
                     for x in range(MAX_RA_BIN)]
@@ -27,21 +46,7 @@ def determine_precision():
             dec_bin_cord = calculate_declination_bin(dec_value)
 
             histogram_3d[RA_bin_cord][dec_bin_cord].append(row)
-
-    bad_points = get_number_of_bad_points(histogram_3d)
-    total_points = MAX_DEC_BIN * MAX_RA_BIN
-    percentage = (bad_points / total_points) * 100
-    RA_to_degrees = (RA_OFFSET / 2) * 15
-
-    os.system('cls')
-    print("----------------Finished computing----------------")
-    print("Right ascension range: {} hrs ({}°)".format(
-        RA_OFFSET / 2, RA_to_degrees))
-    print("Declination range: {}°".format(DEC_OFFSET / 2))
-    print("Total squares that do not have any stars from the database: {}".format(
-        bad_points))
-    print("Total squares the sky was split into: {}".format(total_points))
-    print("Percentage of squares without stars: {}%".format(percentage))
+    return histogram_3d
 
 
 def get_number_of_bad_points(histogram):
